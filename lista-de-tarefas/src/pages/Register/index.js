eec0175ab6e1c6cec628d4f259a843e7289be40a
Diego <149812280+Diegooaraujo@users.noreply.github.com>
2025-08-01 11:42:36 -0300

    test

--- lista-de-tarefas/src/pages/Register/index.js
+++ lista-de-tarefas/src/pages/Register/index.js
@@ -1,11 +1,39 @@
 import React from 'react'
 
 function Register() {
+
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleLogin(e) {
+    e.preventDefault();
+    if (email !== '' && password !== '') {
+
+    } else {
+      alert('Preencha todos os campos!')
+    }
+
+  }
+
+
   return (
-    <div>
-      <h1>Página de Registro</h1>
+    <div className='home-container'>
+      <h1>Lista de tarefas</h1>
+      <span>Gerencie suas tarefas </span>
+
+      <form className='form' onSubmit={handleLogin}>
+        <input type='text' placeholder='Digite seu E-mail' value={email} onChage={(e) => setEmail(e.target.value)} />
+        <input type='password' placeholder='Senha' value={password} onChage={(e) => setPassword(e.target.value)} />
+        <button type='submit'>Acessar</button>
+      </form>
+      <Link className='button-link' to='/register'>Não Possui uma conta? cadastre-se</Link>
+
+
+
     </div>
   )
+
 }
 
 export default Register
